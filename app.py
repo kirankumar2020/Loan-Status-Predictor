b6cb7fbf5289dea740bd38d6dcda0ceab6b6f35e
kirankumar2020 <kale.kk2020@gmail.com>
2024-02-29 22:15:09 +0530

    project file

---
+++ app.py
@@ -0,0 +1,44 @@
+from flask import Flask,request,render_template,url_for,jsonify
+import numpy as np
+import pickle,json
+from utils import get_loan_status
+
+
+
+app = Flask(__name__)
+
+@app.route("/home",methods = ["GET","POST"])
+def home():
+
+    return render_template("index.html")
+
+
+
+@app.route("/loan_status",methods = ["GET","POST"])
+def loan_status():
+    result  = None
+    if request.method == "POST":
+      data_f = request.form
+      Gender = data_f["Gender"]
+      Married = data_f["Married"]
+      Education = data_f["Education"]
+      Self_Employed = data_f["Self_Employed"]
+      ApplicantIncome = int(data_f["ApplicantIncome"])
+      CoapplicantIncome = int(data_f["CoapplicantIncome"])
+      LoanAmount = int(data_f["LoanAmount"])
+      Loan_Amount_Term = int(data_f["Loan_Amount_Term"])
+      Credit_History = float(data_f["Credit_History"])
+      Property_Area = data_f["Property_Area"]
+
+
+      result = get_loan_status(Gender, Married, Education, Self_Employed, ApplicantIncome, CoapplicantIncome, LoanAmount, Loan_Amount_Term,Credit_History,Property_Area)
+    
+      return render_template("index.html",prediction = f"{result}")
+    
+
+    return render_template("index.html")
+  
+
+
+if __name__ == "__main__":
+    app.run()
---
+++ utils.py
@@ -0,0 +1,40 @@
+import pickle
+import numpy as np
+import json
+
+def get_loan_status(Gender, Married, Education, Self_Employed, ApplicantIncome, CoapplicantIncome, LoanAmount, Loan_Amount_Term,Credit_History,Property_Area):
+    mobile_path = r"D:\PDS\Flask\Loan_Data\Loan...KNN_clf_model.pkl"
+    mobile_path_J = r'D:\PDS\Flask\Loan_Data\colum_data1.json'
+
+    with open(mobile_path, 'rb') as f:
+      model = pickle.load(f)
+
+      with open(mobile_path_J, "r") as f:
+       data = json.load(f)
+       Gender = data["Gender"][Gender]
+       Married = data["Married"][Married]
+       Education = data["Education"][Education]
+       Self_Employed = data["Self_Employed"][Self_Employed]
+       Property_Area = data["Property_Area"][Property_Area]
+
+
+
+       test_array = np.zeros((1,model.n_features_in_))
+      # test_array = [Gender, Married, Education, Self_Employed, ApplicantIncome, CoapplicantIncome, LoanAmount, Loan_Amount_Term,Credit_History,Property_Area]
+       
+       
+       test_array[0][0] = Gender
+       test_array[0][1] = Married
+       test_array[0][2] = Education
+       test_array[0][3] = Self_Employed
+       test_array[0][9] = Property_Area
+       predicted_class = model.predict(test_array)[0]
+      # print("predicted_class :",predicted_class)
+
+
+      Loan_status = "Approved" if predicted_class == 1 else "Not Approved"
+      print("Loan Prediction:", Loan_status)
+
+      
+      return Loan_status
+
